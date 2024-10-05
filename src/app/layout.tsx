0c4e0d3233755f482a7fc957171aa0a00c1b8dc4
madbull12 <huangandrian02@gmail.com>
2024-10-05 16:45:28 +0700

    feat: added backgroud snippet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,8 @@ const RootLayout = ({ children }: PropsWithChildren) => {
           <ThemeProvider attribute="class">
             <Navbar />
             {children}
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-white dark:bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
             <ThemeSwitcher className="absolute bottom-5 right-5 z-10" />
             <Footer />
             <Toaster />
