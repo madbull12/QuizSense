bd44e94b8e0e7c7af98b881614676f90b370f6ad
madbull12 <huangandrian02@gmail.com>
2024-10-05 16:47:02 +0700

    feat: added fading text

---
+++ src/components/introduction/heading-fade-hero.tsx
@@ -0,0 +1,20 @@
+'use client';
+import * as React from 'react';
+import * as m from '@/paraglide/messages'
+import TextFade from '@/components/ui/text-fade';
+
+export default function HeadingFadeHero() {
+  return (
+    <TextFade
+      direction="down"
+      className="pt-6 pb-5 flex-col flex justify-center items-center space-y-6"
+    >
+      <h2 className="text-xl text-center sm:text-4xl font-bold  md:text-6xl  max-w-3xl">
+        {m.quizSense_headline()}
+      </h2>
+      <div className="text-center md:text-lg max-w-xl mx-auto text-balance dark:text-zinc-300 font-semibold">
+        {m.quizSense_description()}
+      </div>
+    </TextFade>
+  );
+}
