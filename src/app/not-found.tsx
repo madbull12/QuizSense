1720b65f0cc25cac3b4a80dfa8a065d933fdd141
madbull12 <huangandrian02@gmail.com>
2024-10-06 23:09:30 +0700

    feat: added custom not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button'
+import { Link } from '@/lib/i18n'
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <main className='flex justify-center items-center h-[calc(100vh-75px)] '>
+      <div className='flex flex-col items-center gap-y-2'>
+      <h1 className='text-3xl'>Oops... we can't find the page you're looking for</h1>
+      <Link href="/">
+        Go to home page
+      </Link>
+
+      </div>
+
+    </main>
+  )
+}
+
+export default NotFoundPage
