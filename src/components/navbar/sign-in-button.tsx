da2d22251fb62f907d1f75f400f3fb8ec6757972
madbull12 <huangandrian02@gmail.com>
2024-10-06 23:18:46 +0700

    feat: modified sign in button

--- src/components/navbar/sign-in-button.tsx
+++ src/components/navbar/sign-in-button.tsx
@@ -5,22 +5,15 @@ import { signIn } from 'next-auth/react';
 import * as m from '@/paraglide/messages';
 import MovingArrowButton from '../ui/moving-arrow-button';
 import { HoverBorderGradient } from '../ui/hover-border-gradient';
+import { Link, useRouter } from '@/lib/i18n';
 
 export const SignInButton = () => {
-  const [isPending, startTransition] = useTransition();
-
-  const handleSignIn = () => {
-    startTransition(async () => {
-      await signIn('github');
-    });
-  };
-
+  const router = useRouter();
   return (
-      <MovingArrowButton
-        className="px-4 py-2 gap-x-1 text-sm font-normal"
-        text={m.get_started()}
-      />
+    <MovingArrowButton
+      className="px-4 py-2 gap-x-1 text-sm font-normal"
+      text={m.get_started()}
+      onClick={()=>router.push("/auth/signin")}
+    />
   );
 };
-
-
