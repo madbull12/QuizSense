7431efb26155b8f5daffd85c6714a3d3db401aff
madbull12 <huangandrian02@gmail.com>
2024-10-06 23:16:55 +0700

    style: added animation on rainbow button

--- src/components/introduction/subscribe-button.tsx
+++ src/components/introduction/subscribe-button.tsx
@@ -1,15 +1,23 @@
+"use client"
+
 import React from 'react';
 import { RainbowButton } from '@/components/ui/rainbow-button';
 import { ChevronRight } from 'lucide-react';
-
+import { motion } from 'framer-motion';
+const gradientVariants = {
+  show: { opacity: 1, y: 0, transition: { type: 'spring' } },
+  hidden: { opacity: 0, y: -18 },
+};
 const SubscribeButton = () => {
   return (
-    <RainbowButton className="flex items-center gap-x-2 mx-auto">
-      <span>Subscribe Now</span>
-      <div className="group-hover:translate-x-2 transition-all ">
-        <ChevronRight size={18} />
-      </div>
-    </RainbowButton>
+    <motion.div initial="hidden" animate="show" variants={gradientVariants}>
+      <RainbowButton className="flex items-center gap-x-2 mx-auto">
+        <span>Subscribe Now</span>
+        <div className="group-hover:translate-x-2 transition-all ">
+          <ChevronRight size={18} />
+        </div>
+      </RainbowButton>
+    </motion.div>
   );
 };
 
