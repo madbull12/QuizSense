87d78ea16c3d6a5607092ca1c5420405996bfb15
madbull12 <huangandrian02@gmail.com>
2024-10-06 23:16:12 +0700

    feat: added sign in form

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,13 @@
+import SigninForm from '@/components/auth/signin-form'
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <main className='flex justify-center items-center h-[calc(100vh-80px)]'>
+    <SigninForm />
+
+    </main>
+  )
+}
+
+export default SignInPage
