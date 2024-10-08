e19946217e2a62bc0f3dd0aa1af99fff2aa56899
madbull12 <huangandrian02@gmail.com>
2024-10-08 23:45:19 +0700

    feat: added routes for auth

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+export const publicRoutes = [
+    '/',
+]
+
+export const authRoutes=  [
+    '/auth/signin',
+]
+
+export const apiAuthPrefix = "/api/auth"
+
+export const DEFAULT_LOGIN_REDIRECT="/dashboard"
