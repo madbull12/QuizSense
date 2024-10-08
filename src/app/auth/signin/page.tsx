dc38dd7890e55621ea29149c062020dde475129d
madbull12 <huangandrian02@gmail.com>
2024-10-08 23:48:49 +0700

    feat: added validation on sign in page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,13 +1,18 @@
-import SigninForm from '@/components/auth/signin-form'
-import React from 'react'
+import { auth } from '@/app/api/auth/[...nextauth]/auth';
+import SigninForm from '@/components/auth/signin-form';
+import { redirect } from 'next/navigation';
+import React from 'react';
 
-const SignInPage = () => {
+const SignInPage = async () => {
+  const session = await auth();
+  if (session?.user) {
+    return redirect(`/dashboard`);
+  }
   return (
-    <main className='flex justify-center items-center h-[calc(100vh-80px)]'>
-    <SigninForm />
-
+    <main className="flex justify-center items-center h-[calc(100vh-80px)]">
+      <SigninForm />
     </main>
-  )
-}
+  );
+};
 
-export default SignInPage
+export default SignInPage;
