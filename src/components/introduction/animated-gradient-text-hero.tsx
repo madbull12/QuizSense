cc7c3f3a19f52184b9a6d0555eaec1df18550cb2
madbull12 <huangandrian02@gmail.com>
2024-10-08 23:50:51 +0700

    style: minor changes in styling

--- src/components/introduction/animated-gradient-text-hero.tsx
+++ src/components/introduction/animated-gradient-text-hero.tsx
@@ -17,7 +17,7 @@ const AnimatedGradientHero = () => {
         🎉 <hr className="mx-2 h-4 w-px shrink-0 bg-gray-300" />
         <motion.span
           className={cn(
-            `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`
+            `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent text-xs sm:text-sm md:text-base`
           )}
         >
           Introducing QuizSense
--- src/components/introduction/heading-fade-hero.tsx
+++ src/components/introduction/heading-fade-hero.tsx
@@ -5,16 +5,17 @@ import TextFade from '@/components/ui/text-fade';
 import { useSession } from 'next-auth/react';
 
 export default function HeadingFadeHero() {
-
+  const { data } = useSession()
+  console.log(data)
   return (
     <TextFade
       direction="down"
       className="pt-6 pb-5 flex-col flex justify-center items-center space-y-6"
     >
-      <h2 className="text-xl text-center sm:text-4xl font-bold  md:text-6xl  max-w-3xl">
+      <h2 className="text-2xl text-center sm:text-4xl font-bold  md:text-5xl lg:text-6xl  max-w-3xl">
         {m.quizSense_headline()}
       </h2>
-      <div className="text-center md:text-lg max-w-xl mx-auto text-balance dark:text-zinc-300 font-semibold">
+      <div className="text-center text-xs sm:text-sm md:text-lg max-w-xl mx-auto text-balance dark:text-zinc-300 font-semibold">
         {m.quizSense_description()}
       </div>
     </TextFade>
