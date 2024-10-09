e08ca919d07ecfb8023fd827b8dc968d4f641992
madbull12 <huangandrian02@gmail.com>
2024-10-09 23:52:55 +0700

    style: minor stylings on not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const NotFoundPage = () => {
   return (
-    <main className='flex justify-center items-center h-[calc(100vh-75px)] '>
+    <main className='flex justify-center items-center h-[calc(100vh-75px)] container'>
       <div className='flex flex-col items-center gap-y-2'>
       <h1 className='text-3xl'>Oops... we can't find the page you're looking for</h1>
       <Link href="/">
