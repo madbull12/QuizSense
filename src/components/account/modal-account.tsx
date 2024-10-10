b3160f3a6324cf0967ca6e359a753ff7880ad23a
madbull12 <huangandrian02@gmail.com>
2024-10-10 22:56:02 +0700

    feat: some new changes on modal account

--- src/components/account/modal-account.tsx
+++ src/components/account/modal-account.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Credenza,
   CredenzaClose,
@@ -12,29 +12,43 @@ import {
 import { Settings } from 'lucide-react';
 import * as m from '@/paraglide/messages';
 import ProfileSection from '@/components/account/profile-section';
+import DangerSection from '@/components/account/danger-section';
+import ProfileContent from '@/components/account/profile-content';
+import { useSectionAccount } from '@/store/account';
+
+
 
 const ModalAccount = () => {
+  const section = useSectionAccount((state) => state.section)
   return (
     <Credenza>
       <CredenzaTrigger className="flex items-center">
         <Settings className="mr-2 size-4" /> <span>{m.manage_account()}</span>
       </CredenzaTrigger>
-      <CredenzaContent>
-        <CredenzaHeader>
-          <CredenzaTitle className="text-xl sm:text-2xl md:text-3xl">
-            Account
-          </CredenzaTitle>
-          <CredenzaDescription className="text-xs md:text-sm">
-            Manage your account information
-          </CredenzaDescription>
-        </CredenzaHeader>
-        <ProfileSection />
-
-        <CredenzaFooter>
-          <CredenzaClose asChild>
-            <button>Close</button>
-          </CredenzaClose>
-        </CredenzaFooter>
+      <CredenzaContent className="md:min-h-[500px] md:min-w-[700px] w-full ">
+        {section === '' && (
+          <>
+            <CredenzaHeader>
+              <CredenzaTitle className="text-xl sm:text-2xl md:text-3xl">
+                Account
+              </CredenzaTitle>
+              <CredenzaDescription className="text-xs md:text-sm">
+                Manage your account information
+              </CredenzaDescription>
+            </CredenzaHeader>
+            <ProfileSection  />
+            <CredenzaHeader>
+              <CredenzaTitle className="text-xl sm:text-2xl md:text-3xl">
+                Security
+              </CredenzaTitle>
+              <CredenzaDescription className="text-xs md:text-sm">
+                Manage your security preferences
+              </CredenzaDescription>
+            </CredenzaHeader>
+            <DangerSection />
+          </>
+        )}
+        {section === "profile" && <ProfileContent />}
       </CredenzaContent>
     </Credenza>
   );
