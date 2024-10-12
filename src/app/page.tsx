1bfb8c372b29814f176dc3a80fc9959080706233
madbull12 <huangandrian02@gmail.com>
2024-10-12 23:50:23 +0700

    styling: minor styling on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import IntroductionSection from "@/components/introduction/introduction-section"
 
 const Home = () => {
   return (
-    <section className="container mt-10 flex flex-col items-center gap-3 text-center md:absolute md:left-1/2 md:top-1/2 md:mt-0 md:-translate-x-1/2 md:-translate-y-1/2">
+    <section className="container  flex flex-col items-center justify-center gap-3 text-center min-h-[90vh]">
       <IntroductionSection />
     </section>
   );
