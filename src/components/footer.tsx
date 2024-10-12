9450ce5b1b3034ca024d040a713cfa4e644b7f83
madbull12 <huangandrian02@gmail.com>
2024-10-12 23:51:40 +0700

    style: minor styling on footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,14 @@
 import { Button } from '@/components/ui/button';
+import { ThemeSwitcher } from '@/components/theme-switcher';
 
 export const Footer = () => {
   return (
-    <footer className="text-muted-foreground absolute bottom-2 w-full text-center text-sm">
+    <footer className="text-muted-foreground  w-full text-center text-sm">
       © {new Date().getFullYear()} By{' '}
-      <Button variant="link" className="p-0" asChild>
+      <Button variant="link" className="p-0 mr-auto" asChild>
         <a href="https://andrian-portfolio-five.vercel.app/">Andrian Lysander</a>
       </Button>
+
     </footer>
   );
 };
