c8cc698eb5c1ff5c51e19cf5218750dc10fdce17
madbull12 <huangandrian02@gmail.com>
2024-10-12 23:52:54 +0700

    feat: added bento grid and dashboard grid

---
+++ src/components/dashboard/dashboard-grid.tsx
@@ -0,0 +1,90 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+import { BentoGrid, BentoGridItem } from '../ui/bento-grid';
+import { Flame, History, MessageCircleQuestion } from 'lucide-react';
+import { TypingAnimation } from '../ui/typing-animation';
+// import {
+//   IconArrowWaveRightUp,
+//   IconBoxAlignRightFilled,
+//   IconBoxAlignTopLeft,
+//   IconClipboardCopy,
+//   IconFileBroken,
+//   IconSignature,
+//   IconTableColumn,
+// } from "@tabler/icons-react";
+
+const DashboardGrid = () => {
+  return (
+    <BentoGrid className="max-w-5xl mx-auto">
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          icon={item.icon}
+          className={i === 2 || i === 6 ? 'md:col-span-4' : 'md:col-span-2'}
+        />
+      ))}
+    </BentoGrid>
+  );
+};
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+);
+const items = [
+  {
+    title: 'Quiz Me',
+    description: 'Challenge yourself with a quiz!',
+    header: (
+      <div className="flex flex-col gap-y-2">
+        <TypingAnimation
+          text="The process of preserving a body for the afterlife in Ancient Egypt is called ______________."
+          className="w-full flex-1 text-3xl font-light text-left break-words"
+          duration={30}
+        />
+        <p>Answer is <span className=" font-bold">mummification</span> </p>
+      </div>
+    ),
+    icon: <MessageCircleQuestion size={20} />,
+  },
+  {
+    title: 'History',
+    description: 'View your recent attempts',
+    header: <></>,
+    icon: <History size={20} />,
+  },
+  {
+    title: 'Hot Topics',
+    description: 'Discover hot topics that suit your interests',
+    header: <></>,
+    icon: <Flame size={20} />,
+  },
+  //   {
+  //     title: "The Power of Communication",
+  //     description:
+  //       "Understand the impact of effective communication in our lives.",
+  //     header: <Skeleton />,
+  //     // icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+  //   },
+  //   {
+  //     title: "The Pursuit of Knowledge",
+  //     description: "Join the quest for understanding and enlightenment.",
+  //     header: <Skeleton />,
+  //     // icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+  //   },
+  //   {
+  //     title: "The Joy of Creation",
+  //     description: "Experience the thrill of bringing ideas to life.",
+  //     header: <Skeleton />,
+  //     // icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+  //   },
+  //   {
+  //     title: "The Spirit of Adventure",
+  //     description: "Embark on exciting journeys and thrilling discoveries.",
+  //     header: <Skeleton />,
+  //     // icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+  //   },
+];
+
+export default DashboardGrid;
---
+++ src/components/ui/bento-grid.tsx
@@ -0,0 +1,58 @@
+import { cn } from '@/lib/utils';
+import { ArrowRight, ChevronRight } from 'lucide-react';
+
+export const BentoGrid = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        'grid md:auto-rows-[22rem] grid-cols-1 md:grid-cols-4 gap-4   ',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItem = ({
+  className,
+  title,
+  description,
+  header,
+  icon,
+}: {
+  className?: string;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  header?: React.ReactNode;
+  icon?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        'row-span-1 cursor-pointer rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:border-white/[0.2] bg-transparent border border-transparent justify-between flex flex-col space-y-4',
+        className
+      )}
+    >
+      {header}
+      <div className="flex items-center justify-between transition duration-200">
+        <div className="">
+          {icon}
+          <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+            {title}
+          </div>
+          <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+            {description}
+          </div>
+        </div>
+        <ChevronRight className="group-hover/bento:visible invisible group-hover/bento:translate-x-0 -translate-x-2 transition-all  ease-in-out  " />
+      </div>
+    </div>
+  );
+};
