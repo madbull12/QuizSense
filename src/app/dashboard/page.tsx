4eeb05edca4ffdd8f4250ee6ba030f4181cbe447
madbull12 <huangandrian02@gmail.com>
2024-10-12 23:51:07 +0700

    feat: developed dashboard grid

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { auth } from '@/app/api/auth/[...nextauth]/auth';
 import { env } from '@/env.mjs';
 import { redirect } from 'next/navigation';
+import { Metadata } from 'next';
+import DashboardGrid from '@/components/dashboard/dashboard-grid';
+
+export const metadata: Metadata = {
+  title:"Dashboard",
+  description:"The dashboard page of QuizSense"
+}
 
 const DashboardPage = async () => {
   const session = await auth();
@@ -9,8 +16,9 @@ const DashboardPage = async () => {
     return redirect(`/auth/signin`);
   }
   return (
-    <main className="container p-4">
-      <h1 className="text-3xl font-bold">Welcome to QuizSense</h1>
+    <main className="container p-4 space-y-4">
+      <h1 className="text-lg md:text-2xl font-bold">Welcome to QuizSense</h1>
+      <DashboardGrid />
     </main>
   );
 };
