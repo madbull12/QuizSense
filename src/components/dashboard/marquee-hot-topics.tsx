87b47f8f77a3fc5e9f1a0675a28f0e52b13a834b
madbull12 <huangandrian02@gmail.com>
2024-10-19 23:38:47 +0700

    feat: changed from reviews card to hot topics

--- src/components/dashboard/marquee-hot-topics.tsx
+++ src/components/dashboard/marquee-hot-topics.tsx
@@ -1,58 +1,23 @@
 import { cn } from "@/lib/utils";
 import Marquee from "@/components/ui/marquee";
 
-const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
-  },
-  {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
-  },
-  {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
-  },
+const hotTopics = [
+    "Quantum Computer",
+    "Rust Programming",
+    "Artificial Intelligence",
+    "Cloud Computing",
+    "Internet of Things",
+    "Cyber Security",
+    
 ];
 
-const firstRow = reviews.slice(0, reviews.length / 2);
-const secondRow = reviews.slice(reviews.length / 2);
+const firstRow = hotTopics.slice(0, hotTopics.length / 2);
+const secondRow = hotTopics.slice(hotTopics.length / 2);
 
-const ReviewCard = ({
-  img,
-  name,
-  username,
-  body,
+const HotTopicCard = ({
+  topic,
 }: {
-  img: string;
-  name: string;
-  username: string;
-  body: string;
+    topic:string
 }) => {
   return (
     <figure
@@ -64,16 +29,7 @@ const ReviewCard = ({
         "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
       )}
     >
-      <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
-        <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
-        </div>
-      </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
+     {topic}
     </figure>
   );
 };
@@ -82,13 +38,13 @@ export function MarqueeHotTopics() {
   return (
     <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <Marquee pauseOnHover className="[--duration:20s]">
-        {firstRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+        {firstRow.map((topic,idx) => (
+          <HotTopicCard key={idx} topic={topic}  />
         ))}
       </Marquee>
       <Marquee reverse pauseOnHover className="[--duration:20s]">
-        {secondRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+        {secondRow.map((topic,idx) => (
+          <HotTopicCard key={hotTopics.length + idx} topic={topic}  />
         ))}
       </Marquee>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
